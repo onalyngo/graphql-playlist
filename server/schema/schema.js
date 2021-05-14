eb9b1eda3b93ef0e98fef2ce700ed5ed38c232a7
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-05-14 13:54:40 +0800

    GraphQL ID Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql');
 const _ = require('lodash');
 
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
 
 // dummy data
 var books = [
@@ -14,25 +14,26 @@ var books = [
 const BookType = new GraphQLObjectType({
 	name:'Book',
 	fields:() => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString }
 	})
 });
 
 const RootQuery = new GraphQLObjectType({
-    name:'RootQueryType',
-    fields: {
-        book: {
-            type: BookType,
-            args: { id:{ type: GraphQLString }},
-            resolve(parent, args){
-                // code to get data from db / other source
-                return _.find(books, { id: args.id });
-            }
-        }
-    }
-});
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				// code to get data from db / other source
+                console.log(typeof(args.id))
+				return _.find(books, { id: args.id })
+			},
+		},
+	},
+})
 
 module.exports = new GraphQLSchema({
     query: RootQuery
