edaf04e185d05dd85198ba65ec9031d8e93c7b00
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-05-14 16:45:04 +0800

    type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,10 +6,10 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt }
 
 // dummy data
 var books = [
-	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
-];
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+]
 
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
@@ -22,7 +22,14 @@ const BookType = new GraphQLObjectType({
 	fields:() => ({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
-		genre: { type: GraphQLString }
+		genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                console.log(parent);
+                return _.find(authors, { id: parent.authorId })
+            }
+        }
 	})
 });
 
@@ -43,7 +50,7 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// code to get data from db / other source
-				console.log(typeof args.id)
+				// console.log(typeof args.id)
 				return _.find(books, { id: args.id })
 			}
 		},
