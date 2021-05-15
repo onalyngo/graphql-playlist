d707f225b3cfb515328cf67528cb40c60b3abff4
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-05-15 17:00:46 +0800

    connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 
 const app = express()
+
+// connect to mongodb database
+// make sure to replace my db string & creds with your own
+mongoose.connect(
+	'mongodb+srv://admin:admin123@cluster0.rgirv.mongodb.net/gql-ninja?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+)
+.then(() => {
+	console.log('Connected to database');
+});
+
 
 //middleware
 app.use('/graphql', graphqlHTTP({
