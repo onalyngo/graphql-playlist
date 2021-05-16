60ae644d67ec202eee32eb569984e20dd8538630
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-05-16 21:45:04 +0800

    update the resolve functions

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,6 +17,7 @@ const BookType = new GraphQLObjectType({
             resolve(parent, args){
                 // console.log(parent);
                 // return _.find(authors, { id: parent.authorId })
+				return Author.findById(parent.authorId);
             }
         }
 	})
@@ -32,6 +33,7 @@ const AuthorType = new GraphQLObjectType({
 			type: GraphQLList(BookType),
             resolve(parent, args){
                 // return _.filter(books, { authorId: parent.id });
+				return Book.find({ authorId: parent.id });
             }
 		},
 	}),
@@ -47,6 +49,7 @@ const RootQuery = new GraphQLObjectType({
 				// code to get data from db / other source
 				// console.log(typeof args.id)
 				// return _.find(books, { id: args.id })
+				return Book.findById(args.id);
 			}
 		},
 		author: {
@@ -54,18 +57,21 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// return _.find(authors, { id: args.id });
+				return Author.findById(args.id);
 			}
 		},
 		books: {
 			type: new GraphQLList(BookType),
 			resolve(parent, args){
 				// return books
+				return Book.find({});
 			}
 		},
 		authors: {
 			type: new GraphQLList(AuthorType),
 			resolve(parent, args){
 				// return authors;
+				return Author.find({});
 			}
 		}
 	},
