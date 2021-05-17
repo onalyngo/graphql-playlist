183a21d8bbd9faef8ca808ae8db86defb4a2e289
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-05-17 09:27:45 +0800

    set up apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-
+import { ApolloClient, ApolloProvider } from '@apollo/client';
 // components
-import BookList from './components/BookList';
+import BookList from './components/BookList'
 
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+})
 
 function App() {
-  return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-      <BookList />
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div id='main'>
+				<h1>Ninja's Reading List</h1>
+				<BookList />
+			</div>
+		</ApolloProvider>
+	)
 }
 
-export default App;
+export default App
